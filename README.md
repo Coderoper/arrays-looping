e99749d02dbb28440d071a2bb62cd5a15017c158
Austin Roberts <d.austin.roberts@gmail.com>
2018-03-30 15:55:02 -0700

    This will replace each 0 in an array with a Beep

---
+++ README.md
@@ -0,0 +1,40 @@
+# _Beep Boop Machine_
+
+#### _Application that takes user inputed integer and outputs a string counting up to that with words and phrases taking place of numbers with specified properties, {3/30/18}_
+
+#### By _**Alex Noble, David Tumpowsky**_
+
+## Description
+A program that takes a user inputted integer and creates a string counting up to that number and replacing specified numbers with words and phrases
+
+Specs
+* 1. Replace every 0 in the count with a Beep unless spec 2 applies
+* 2. Replace every 1 in the count with a Boop unless spec 3 applies
+* 3. Replace all numbers divisible by 3 with "I'm sorry Dave, I can't let you do that"
+
+example input: "11"
+example output:
+
+Boop2"I'm sorry Dave, I can't let you do that"45"'m sorry Dave, I can't let you do that"78"I'm sorry Dave, I can't let you do that"Boop0BoopBoop
+
+
+
+
+## Setup/Installation Requirements
+
+* Open in a web browser.
+
+## Known Bugs
+
+_No known bugs_
+
+
+## Technologies Used
+
+_HTML, CSS, Javascript, Jquery, and Bootstrap_
+
+### License
+
+*MIT License*
+
+Copyright (c) 2018 **_Austin Roberts_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,56 @@
 //Backend logic
-function beepBoop(input){
-  console.log(input)
-  if (isNaN(result)) {
+var array =[];
+var input;
+var zeroes;
+var beepStr;
+var beepArr=[];
+var replaceBeep;
+
+function createArray(input){
+  if (isNaN(input)) {
     alert("Please enter some integer")
     document.getElementById("form").reset();
-
   } else {
-    return input
+      for (var index = 1; index <= input; index += 1) {
+        array.push(index);
+      }
   };
-  };
+  return array;
+};
+function swapNumbers(array) {
+  for (var index=1; index <= array.length; index++) {
+    // console.log(index);
+    var beepStr=index.toString();
+    // console.log(beepStr);
+    var replaceBeep = beepStr.replace(0, "Beep");
+    // console.log(replaceBeep)
+    beepArr.push(replaceBeep);
+    console.log(beepArr);
+  }
+  return beepArr
+
+};
+//
+// array.forEach(function(element) {
+//   console.log("look" +element);
+// });
+// //   return
+// //   array.forEach(function(part, index, newArray) {
+// //   newArray[index] = "hello world";
+// // });
+//
+// };
+
+
 
 //Front end logic
 $(document).ready(function() {
   $("#form").submit(function(event) {
       event.preventDefault();
       var input = parseInt($("#input").val());
-      console.log("hello")
-      console.log(input);
-      $("#result").text(beepBoop(input))
-      document.getElementById("form").reset();
 
+      $("#result").text(createArray(input));
+      $("#result2").text(swapNumbers(array));
+      document.getElementById("form").reset();
     });
 });
